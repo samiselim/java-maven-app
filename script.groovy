b08212f2f2adee03183c2ff1326fb6d34a37cd2a
samiselim <samiselim75@gmail.com>
2024-01-16 13:33:22 +0200

    Bulding and deploying maven java project using jenkins file and groovy #15

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'sami_docker_hub_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t docker samiselim/demo-java-maven-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push samiselim/demo-java-maven-app:jma-3.0'
     }
 } 
 
