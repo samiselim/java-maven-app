406996c57e8908f5fee8650ec6c2c6b35eec418a
samiselim <samiselim75@gmail.com>
2024-01-18 06:26:58 +0200

    Editing some errors #6

--- script.groovy
+++ script.groovy
@@ -35,7 +35,7 @@ def commitChanges(){
       
         sh 'whoami'
 
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/samiselim/java-maven-app.git"
+        sh "git remote set-url origin https://jenkins:${PASS}@github.com/samiselim/java-maven-app.git"
         sh 'git add .'
         sh 'git commit -m "this commit from jenkins "'
         sh 'git push origin HEAD:jenkins-update'
