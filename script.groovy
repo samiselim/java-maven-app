2ac3bce123666091bfa8f361cac4e3d9f2d57332
samiselim <samiselim75@gmail.com>
2024-01-18 06:30:53 +0200

    Editing some errors #6

--- script.groovy
+++ script.groovy
@@ -31,11 +31,11 @@ def deployApp() {
 
 def commitChanges(){
     echo "Committing changes to github repository"
-     withCredentials([usernamePassword(credentialsId: 'sami_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+     withCredentials([usernamePassword(credentialsId: 'sami_githubAcess', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
       
         sh 'whoami'
 
-        sh "git remote set-url origin https://jenkins:${PASS}@github.com/samiselim/java-maven-app.git"
+        sh "git remote set-url origin https://${USER}:${PASS}@github.com/samiselim/java-maven-app.git"
         sh 'git add .'
         sh 'git commit -m "this commit from jenkins "'
         sh 'git push origin HEAD:jenkins-update'
