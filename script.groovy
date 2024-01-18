843a87ce32b79a7e01ee8a56eaae7fa10999d903
samiselim <samiselim75@gmail.com>
2024-01-18 03:25:19 +0200

    Adding Versioning Control to the application #2

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'sami_docker_hub_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t samiselim/demo-java-maven-app:$IMAGE_VERSION ."
+        sh "docker build -t samiselim/demo-java-maven-app:${IMAGE_VERSION} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push samiselim/demo-java-maven-app:$IMAGE_VERSION"
+        sh "docker push samiselim/demo-java-maven-app:${IMAGE_VERSION}"
     }
 } 
 def incVersion(){
