fea037d8a564b493aadf7657ae8ca7c73d933b9e
samiselim <samiselim75@gmail.com>
2024-01-18 03:36:07 +0200

    Adding Versioning Control to the application #6

--- script.groovy
+++ script.groovy
@@ -13,7 +13,7 @@ def buildImage() {
 } 
 def incVersion(){
     echo "Incrementing App Version"
-    sh "mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion} versions:commit"
+    sh 'mvn build-helper:parse-version version-set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'  // this code is reguler expression to extract version tag fro xml file 
     def version = matcher[0][1] // to extract the text of version only 
     env.IMAGE_VERSION = "$version-$BUILD_NUMBER"
