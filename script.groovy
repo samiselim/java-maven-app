e84408081d12a392dcea359500d0e7184607582e
samiselim <samiselim75@gmail.com>
2024-01-18 06:25:09 +0200

    Editing some errors #6

--- script.groovy
+++ script.groovy
@@ -33,7 +33,7 @@ def commitChanges(){
     echo "Committing changes to github repository"
      withCredentials([usernamePassword(credentialsId: 'sami_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
       
-
+        sh 'whoami'
 
         sh "git remote set-url origin https://${USER}:${PASS}@github.com/samiselim/java-maven-app.git"
         sh 'git add .'
