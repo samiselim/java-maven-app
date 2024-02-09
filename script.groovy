d50293fa071198e4eebc418c88e8793826ee7d30
samiselim <samiselim75@gmail.com>
2024-02-09 16:55:48 +0200

    Adding Authentication to Dockerhub to EC2

--- script.groovy
+++ script.groovy
@@ -12,7 +12,7 @@ def incVersion(){
 }
 def provisionServer(){
     dir('terraform_config'){
-        sh "terraform init"
+        // sh "terraform init"
         sh "terraform destroy --auto-approve"
         EC2_PUBLIC_IP = sh(script: "terraform output ec2_public_ip" , returnStdout: true).trim()
     }
